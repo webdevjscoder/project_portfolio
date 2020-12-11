89fac5c5700a8634a6375e68340813108722e4f3
Juan Cobian <cobianjuan19@gmail.com>
2020-12-10 16:00:41 -0800

    add work projects with links to github repos

--- src/work.js
+++ src/work.js
@@ -12,6 +12,54 @@ function Work() {
             </div>
             <div>
                 {/*  projects links go here in different divs  */}
+                <a href="https://github.com/webdevjscoder/game_reviews">
+                    <div>
+                        <h3>Game Reviews</h3>
+                        <p>
+                            Game reviews is design to provide users the information many gamers desire before purchasing
+                            a game. Whether its the professional unbiased opinions, information, or ratings.
+                        </p>
+                    </div>
+                </a>
+                <a href="https://github.com/webdevjscoder/world_sports_news">
+                    <div>
+                        <h3>World Sports News</h3>
+                        <p>
+                            Wanna know how your favorite team is doing this season? Or do you wanna know is your
+                            favorite player is playing this week? Well with world sports news you get all that and more.
+                        </p>
+                    </div>
+                </a>
+                <a href="https://github.com/webdevjscoder/car_dealership_rails">
+                    <div>
+                        <h3>Bizarre Car Dealership</h3>
+                        <p>
+                            Bizarre car dealership is the ultimate site for car buying. Who wants to wait hours at a
+                            dealership purchasing a new car? At Bizarre car dealership the user is presented with a fast
+                            and easy buying experience.
+                        </p>
+                    </div>
+                </a>
+                <a href="https://github.com/webdevjscoder/feed-your-cravings-frontend">
+                    <div>
+                        <h3>Feed Your Cravings</h3>
+                        <p>
+                            Craving something that probably doesn't exist? Well look no further, here at feed your
+                            cravings a user is provided with unique and unbelievable dishes. With the ability to craft
+                            your own dish for others to try for themselves.
+                        </p>
+                    </div>
+                </a>
+                <a href="https://github.com/webdevjscoder/cece_pastry_creations_frontend">
+                    <div>
+                        <h3>Cece Pastry Creations</h3>
+                        <p>
+                            Cece pastry creations is a bakery application for users who are looking to purchase for an
+                            upcoming event. From birthday cakes to wedding cakes and everything in between cece pastry
+                            creations has you covered.
+                        </p>
+                    </div>
+                </a>
             </div>
         </div>
     )
