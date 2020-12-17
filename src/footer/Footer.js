e7c51acfb3d76b6d14718e0e0d137d78fc1fbdf0
Juan Cobian <cobianjuan19@gmail.com>
2020-12-17 01:59:17 -0800

    create sticky footer

---
+++ src/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../src/App.css';
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <footer className='footer'>
+                Created by Webdevjscoder &#169; 2020
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
